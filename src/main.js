fcd5f0d67dc314374b4c8ef598509e6327a6ac2b
OnedGmez <oned.gomez@unah.hn>
2024-03-20 20:09:07 -0600

    import del i18n para levantar la página y que permita las traducciones

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import i18n from './lang/i18next.config.js'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = i18n(createApp(App))
 
 app.use(createPinia())
 app.use(router)
